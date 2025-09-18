19081b0a4cb9ac045bf6d7fd15d45c9e1bb6c515
Ming <2847919699@qq.com>
2025-09-18 13:05:14 +0800

    GatewayFilter 路由过滤器，作用于任意指定的路由；默认不生效，要配置到路由后生效 创建过滤器工厂，读取配置创建过滤器对象

---
+++ hm-gateway/src/main/java/com/hmalll/gateway/filters/PrintAnyGateFilterFactory.java
@@ -0,0 +1,53 @@
+package com.hmalll.gateway.filters;
+
+import lombok.Data;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class PrintAnyGateFilterFactory extends AbstractGatewayFilterFactory<PrintAnyGateFilterFactory.Config> {
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return new OrderedGatewayFilter(new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                String a = config.getA();
+                String b = config.getB();
+                String c = config.getC();
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+                System.out.println("c = " + c);
+
+                ServerHttpRequest request = exchange.getRequest();
+                System.out.println("print any filter running");
+                return chain.filter(exchange);
+            }
+        },1);
+    }
+
+    @Data
+    public static class Config {
+    private String a;
+    private String b;
+    private String c;
+    }
+
+    public PrintAnyGateFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("a","b","c");
+    }
+}
